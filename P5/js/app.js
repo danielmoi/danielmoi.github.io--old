5b3ac4abefb5d45d9e3543fc2b6e3257940a91f1
dm4000 <daniel@listretto.com>
2016-01-08 13:53:58 +0900

    Initial commit
    Attempt to make maps work

---
+++ P5/js/app.js
@@ -0,0 +1,21 @@
+
+
+var map;
+
+// there are 2 required options for every map: `center` and `zoom`
+// the JS class that represents a map is the `Map` class
+// objects of this class define a single map on a page
+// We may create more than one instance of this class
+// We create a new instance of this class using the `new` operator
+
+// This code defines a variable `map`, and assigns its value to a new `Map` object
+// The function `Map()` is a `constructor`
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: -34.397,
+      lng: 150.644
+    },
+    zoom: 8
+  });
+}
