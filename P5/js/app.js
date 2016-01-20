29729e5cdc0b14185b7f2400ff83559241905ffd
dm4000 <daniel@listretto.com>
2016-01-20 15:51:23 +1100

    Add second marker and zoom map to fit bounds
    Playing around ages with bounds (which I should have more usefully
    called LatLngBounds), and Constructors

--- P5/js/app.js
+++ P5/js/app.js
@@ -21,8 +21,9 @@ var ViewModel = function () {
   self.marker = new google.maps.Marker({
     position: self.mapOptions.center, // object literal with 2 properties, lat & lng
     map: self.myMap, // my map is called 'map'
-    title: "WASSUP!" // what displays upon hover
+    title: "WASSUUUP!" // what displays upon hover
   });
+
 
 
 
@@ -31,55 +32,89 @@ var ViewModel = function () {
   //  self.service = new google.maps.places.PlacesService(self.myMap);
 
 
-  self.venues_object = ko.observable();
-  self.venue_name_array = ko.observableArray();
-  self.venue_location = ko.observableArray();
 
+  // Create empty objects for Foursquare
   self.explore_object = ko.observable();
   self.explore_object_photos = ko.observableArray();
 
-  self.googlePlacesArray = ko.observableArray();
+  // create empty objects for map
+  self.marker2 = ko.observable();
 
+
+  // set starting city
   self.city = ko.observable("Sydney");
 
+
+  // Foursquare stuff
   self.start = "https://api.foursquare.com/v2/venues/explore?";
   self.client_id = "client_id=J4JTA0KKSKB50R1ONPYB3W4H532SPS403IHJKL4VQMNMNKT0";
   self.client_secret = "&client_secret=W5FBT3FTE1X4RVJXPSJJDNNXCYHXL0OMH1TPVINZ40NO0LX5";
-
   self.location = ko.computed(function () {
     return "&near=" + self.city();
   });
-
-
-
-
   self.limit = "&limit=10";
   self.v = "&v=20140806";
   self.m = "&m=foursquare";
 
+
+  // the GO button
   self.getStuff = function () {
     $.ajax({
       url: self.start + self.client_id + self.client_secret + self.location() + self.v + self.m + self.limit + '&section=coffee&venuePhotos=1',
 
       success: function (returnedData) {
-        
-        
+
+
         self.explore_object_photos([]);
-        for (var i = 0; i < returnedData.response.groups[0].items.length; i++) {
+
+        var fsdata = returnedData.response.groups[0].items;
+
+        for (var i = 0; i < fsdata.length; i++) {
 
           self.explore_object_photos.push(
-            returnedData.response.groups[0].items[i].venue.featuredPhotos.items[0].prefix +
+            fsdata[i].venue.featuredPhotos.items[0].prefix +
             'width400' +
-            returnedData.response.groups[0].items[i].venue.featuredPhotos.items[0].suffix
-
-
-
+            fsdata[i].venue.featuredPhotos.items[0].suffix
           );
 
-        self.explore_object(returnedData.response.groups[0].items);
 
-          
-        console.log(returnedData.response.groups[0].items);
+          console.log(fsdata[i].venue.location.lat);
+
+
+
+
+        }
+
+        self.marker3 = new google.maps.Marker({
+          position: {
+            lat: 35.6,
+            lng: 139.7
+          },
+          map: self.myMap, // my map is called 'map'
+          title: "FINALLY!" // what displays upon hover
+
+        });
+
+        self.bounds = new google.maps.LatLngBounds();
+        
+        console.log(self.bounds.toString());
+        // (1, 180), (-1, -180)
+        
+        self.bounds.extend(new google.maps.LatLng(self.mapOptions.center.lat, self.mapOptions.center.lng));
+
+        self.bounds.extend(new google.maps.LatLng(self.marker3.position.lat(), self.marker3.position.lng()));
+        console.log(self.marker3);
+        console.log(self.marker3.position.lat());
+        console.log(self.bounds.toString());
+        
+        self.myMap.fitBounds(self.bounds);
+
+
+        self.explore_object(fsdata);
+
+
+        console.log(fsdata);
+        console.log(self.marker3.position.lat());
 
       }
     });
