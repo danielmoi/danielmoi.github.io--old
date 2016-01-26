4e4607d4b20fc5f96b92ad3cb129234fbb642bf3
dm4000 <daniel@listretto.com>
2016-01-26 17:39:21 +1100

    Restructure project folders and gulp
    Need to clean 'dist' before running gulp, and need to sort out the
    1. min.css in html
    2. copying files etc

--- P5/gulpfile.js
+++ P5/gulpfile.js
@@ -5,30 +5,44 @@ var gulp = require('gulp'),
 minifyCSS = require('gulp-minify-css');
 
 
+// Gulp html files
+gulp.task('html', function() {
+  gulp.src('src/index.html')
+    .pipe(gulp.dest('dist'));
+});
 
+// Gulp image files
+gulp.task('images', function() {
+  gulp.src('src/img/')
+    .pipe(gulp.dest('dist/img'));
+});
 
 // Gulp script files – minify and rename
 gulp.task('scripts', function () {
-  gulp.src('js/app.js')
+  gulp.src('src/js/app.js')
     .pipe(uglify())
     .pipe(rename('app.min.js'))
-    .pipe(gulp.dest('js/'));
+    .pipe(gulp.dest('dist/js/'));
+  gulp.src('src/js/lib/*')
+    .pipe(gulp.dest('dist/js/lib'));
 });
 
 // Gulp style files – minify and rename
 gulp.task('styles', function () {
-  gulp.src('css/style.css')
+  gulp.src('src/css/style.css')
     .pipe(minifyCSS())
     .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('css/'));
+    .pipe(gulp.dest('dist/css/'));
+  gulp.src('src/css/lib/*')
+    .pipe(gulp.dest('dist/css/lib'));  
 });
 
 
 // Watch stuff
 gulp.task('watch', function() {
-  gulp.watch('js/app.js', ['scripts']);
-  gulp.watch('css/style.css', ['styles']);
+  gulp.watch('src/js/app.js', ['scripts']);
+  gulp.watch('src/css/style.css', ['styles']);
 });
 
 // Run gulp
-gulp.task('default', ['scripts', 'styles']);
+gulp.task('default', ['html', 'scripts', 'styles', 'images']);
