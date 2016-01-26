09b0ca302209712e1956a7260a84569ac544561f
dm4000 <daniel@listretto.com>
2016-01-27 08:49:29 +1100

    Improve gulpfile

--- P5/gulpfile.js
+++ P5/gulpfile.js
@@ -2,40 +2,72 @@
 var gulp = require('gulp'),
   uglify = require('gulp-uglify'),
   rename = require('gulp-rename'),
-minifyCSS = require('gulp-minify-css');
+  cssnano = require('gulp-cssnano'),
+  del = require('del'),
+  notify = require('gulp-notify');
 
+// Clean up first!
+gulp.task('clean', function () {
+  // this uses a Node module directly within gulp (no need for gulp plugin)
+  return del(['dist']);
+});
 
 // Gulp html files
-gulp.task('html', function() {
-  gulp.src('src/index.html')
-    .pipe(gulp.dest('dist'));
+gulp.task('html', function () {
+  return gulp.src('src/index.html') // add return so it's asynchronous
+    .pipe(gulp.dest('dist'))
+    .pipe(notify({
+      message: 'html task complete'
+    }));
 });
 
 // Gulp image files
+gulp.task('images', function () {
+  return gulp.src('src/img/**/*.*')
+    .pipe(gulp.dest('dist/img'))
+    .pipe(notify({
+      message: 'images task complete'
+    }));
+});
 
 
 // Gulp script files – minify and rename
 gulp.task('scripts', function () {
-  gulp.src('src/js/app.js')
+  return gulp.src('src/js/**/*.js') // need to add "**/" to make this recursive
     .pipe(uglify())
-    .pipe(rename('app.min.js'))
-    .pipe(gulp.dest('dist/js/'));
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('dist/js')) // trailing "/" not needed
+    .pipe(notify({
+      message: 'scripts task complete'
+    }));
 });
 
 // Gulp style files – minify and rename
 gulp.task('styles', function () {
-  gulp.src('src/css/style.css')
-    .pipe(minifyCSS())
-    .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('dist/css/'));
+  return gulp.src('src/css/**/*.css')
+    .pipe(cssnano())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(notify({
+      message: 'styles task complete'
+    }));
+
 });
 
 
 // Watch stuff
-gulp.task('watch', function() {
-  gulp.watch('src/js/app.js', ['scripts']);
-  gulp.watch('src/css/style.css', ['styles']);
+gulp.task('watch', function () {
+  gulp.watch('src/js/**/*.js', ['scripts']);
+  gulp.watch('src/css/**/*.css', ['styles']);
+  gulp.watch('src/css/**/*.*', ['images']);
+  gulp.watch('src/index.html', ['html']);
 });
 
 // Run gulp
-gulp.task('default', ['html', 'scripts', 'styles']);
+gulp.task('default', ['clean'], function () {
+  gulp.start('html', 'images', 'scripts', 'styles');
+});
