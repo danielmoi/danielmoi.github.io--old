3e31d09126f2734457dcf1e7a3cd982769091f94
danielmoi <daniel@ampersandmoi.com>
2016-02-01 09:17:53 +1100

    New suite: New Feed Selection (#15)

--- P6/jasmine/spec/feedreader.js
+++ P6/jasmine/spec/feedreader.js
@@ -108,9 +108,13 @@ $(function () {
 
       done();
     });
+  });
 
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+    /* TODO: Write a new test suite named "New Feed Selection" */
+  describe('New Feed Selection', function () {
+    
+
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
