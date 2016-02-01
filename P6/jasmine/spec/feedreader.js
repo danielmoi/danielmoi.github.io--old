264a71339c06bd9fb5d1ac4dc7b2dd119d6de697
danielmoi <daniel@ampersandmoi.com>
2016-02-01 14:00:47 +1100

    Refactor To Do lines

--- P6/jasmine/spec/feedreader.js
+++ P6/jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@ $(function () {
     });
 
 
-    /* TODO: Write a test that loops through each feed
+    /* A test that loops through each feed
      * in the allFeeds object and ensures it has a URL defined
      * and that the URL is not empty.
      */
@@ -41,7 +41,7 @@ $(function () {
 
 
 
-    /* TODO: Write a test that loops through each feed
+    /* A test that loops through each feed
      * in the allFeeds object and ensures it has a name defined
      * and that the name is not empty.
      */
@@ -78,7 +78,7 @@ $(function () {
 
   });
 
-  /* TODO: Write a new test suite named "The menu" */
+  /* A new test suite named "The menu" */
   describe('The Menu', function () {
     /* TODO: Write a test that ensures the menu element is
      * hidden by default. You'll have to analyze the HTML and
@@ -92,7 +92,7 @@ $(function () {
 
 
 
-    /* TODO: Write a test that ensures the menu changes
+    /* A test that ensures the menu changes
      * visibility when the menu icon is clicked. This test
      * should have two expectations: does the menu display when
      * clicked and does it hide when clicked again.
@@ -111,10 +111,10 @@ $(function () {
   });
 
 
-  /* TODO: Write a new test suite named "Initial Entries" */
+  /* A new test suite named "Initial Entries" */
   describe('Initial Entries', function () {
 
-    /* TODO: Write a test that ensures when the loadFeed
+    /* A test that ensures when the loadFeed
      * function is called and completes its work, there is at least
      * a single .entry element within the .feed container.
      * Remember, loadFeed() is asynchronous so this test will require
@@ -138,15 +138,12 @@ $(function () {
   });
 
 
-  /* TODO: Write a new test suite named "New Feed Selection" */
+  /* A new test suite named "New Feed Selection" */
   describe('New Feed Selection', function () {
 
 
 
-    /* TODO: Write a test that ensures when a new feed is loaded
-     * by the loadFeed function that the content actually changes.
-     * Remember, loadFeed() is asynchronous.
-     */
+
 
 
 
@@ -168,7 +165,10 @@ $(function () {
       });
     });
 
-
+    /* A test that ensures when a new feed is loaded
+     * by the loadFeed function that the content actually changes.
+     * Remember, loadFeed() is asynchronous.
+     */
 
     it('when new feed is loaded, that the content actually changes', function (done) {
       loadFeed(myIndex, function () {
@@ -181,12 +181,16 @@ $(function () {
         expect(contentBefore).not.toMatch(contentAfter);
         console.log('loadFeed in `it(content)` completed');
         done();
-      console.log('code outside of loadFeed in `it(content)` completed');
-        
+        console.log('code outside of loadFeed in `it(content)` completed');
+
       });
 
     });
 
+    /* A test that ensures when a new feed is loaded
+     * by the loadFeed function that the header color changes.
+     * Remember, loadFeed() is asynchronous.
+     */
     it('when new feed is loaded, that the header color changes',
       function (done) {
         loadFeed(myIndex, function () {
@@ -198,10 +202,10 @@ $(function () {
           console.log('loadFeed in `it(color)` completed');
           done();
         });
-      console.log('code outside of loadFeed in `it(color)` completed');
+        console.log('code outside of loadFeed in `it(color)` completed');
       });
 
-    afterEach(function () {
+    afterAll(function () {
       loadFeed(0);
       console.log('loadFeed in `afterEach` completed');
     });
